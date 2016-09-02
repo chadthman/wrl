9706b7fd5f5e293f4a7f2557d340089530f0b356
Philip Song <psong@sofi.org>
2016-09-02 08:52:00 -0700

    Add Integration Test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -19,8 +19,9 @@ public class IntegrationTest {
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                browser.goTo("http://localhost:9000");
+                assertThat(browser.title()).isEqualTo("WRL: Will Read Later");
+                assertThat(browser.pageSource()).contains("Add Bookmark");
             }
         });
     }
